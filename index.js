cc796215e6efba481b79859c4e5108457339c26a
Ricardo Rodriguez Teamo <rickenofficial@gmail.com>
2024-07-01 14:48:30 -0600

    fix errors on index.js, fix prompt for inquirer, add inputs for the user, add validations for empty strings, add links for renderLicenseLink, add the body of content of the readme file, export the modules

--- index.js
+++ index.js
@@ -1,13 +1,79 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { generateMarkdown } = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
 
+    
+    {
+        type: "input",
+        message: "What is your project title?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "Provide a short description of your project.",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "What are the steps required to install your project?",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "Provide instructions and examples for use.",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "Provide contribution guidelines.",
+        name: "contributing",
+    },
+    {
+        type: "input",
+        message: "Provide test instructions.",
+        name: "tests",
+    },
+    {
+        type: "list",
+        message: "Choose a license for your project.",
+        name: "license",
+        choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None']
+    },
+    {
+        type: "input",
+        message: "Enter your GitHub username.",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "Enter your email address.",
+        name: "email",
+    }
+
+
+];
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile('README.md', data, (err) =>
+        err ? console.error(err) : console.log(`${fileName} has been generated successfully!`)
+    );
+}
+
+
+
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const markdown = generateMarkdown(answers);
+        writeToFile('README.md', markdown);
+    });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,122 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'None') {
+    return '';
+  }
+  
+  let licenseBadge = '';
+  switch (license) {
+    case 'MIT':
+      licenseBadge = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+      break;
+    case 'GPLv3':
+      licenseBadge = '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+      break;
+    case 'Apache 2.0':
+      licenseBadge = '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+      break;
+    case 'BSD 3-Clause':
+      licenseBadge = '![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)';
+      break;
+    default:
+      licenseBadge = '';
+      break;
+  }
+  return licenseBadge;
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'None') {
+    return '';
+  }
+  
+  let licenseLink = '';
+  switch (license) {
+    case 'MIT':
+      licenseLink = 'https://opensource.org/licenses/MIT';
+      break;
+    case 'GPLv3':
+      licenseLink = 'https://www.gnu.org/licenses/gpl-3.0';
+      break;
+    case 'Apache 2.0':
+      licenseLink = 'https://www.apache.org/licenses/LICENSE-2.0';
+      break;
+    case 'BSD 3-Clause':
+      licenseLink = 'https://opensource.org/licenses/BSD-3-Clause';
+      break;
+    default:
+      licenseLink = '';
+      break;
+  }
+  return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'None') {
+    return '';
+  }
+
+  return `
+## License
+This project is licensed under the ${license} license. For more information, please visit the [license link](${renderLicenseLink(license)}).
+  `;
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+  
   return `# ${data.title}
 
+  ${licenseBadge}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+${licenseSection}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions, please feel free to contact me:
+- GitHub: [${data.github}](https://github.com/${data.github})
+- Email: ${data.email}
 `;
 }
 
-module.exports = generateMarkdown;
+
+//exporting modules
+module.exports = {
+  renderLicenseBadge,
+  renderLicenseLink,
+  renderLicenseSection,
+  generateMarkdown
+};
